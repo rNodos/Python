c0824b6183458897b525b7c61f51e41d42c671ce
rNodos <93883322+rNodos@users.noreply.github.com>
2025-02-16 13:10:25 +0000

    simple input and output

---
+++ Tests/input_output.py
@@ -0,0 +1,14 @@
+# input a float value for variable a here
+a = float(input("Añade un número: "))
+# input a float value for variable b here
+b = float(input("Añade otro número: "))
+# output the result of addition here
+print("La suma de ", a, " y ", b, "es = ", a + b)
+# output the result of subtraction here
+print("La resta de ", a, " y ", b, "es = ", a - b)
+# output the result of multiplication here
+print("La multiplicación de ", a, " y ", b, "es = ", a * b)
+# output the result of division here
+print("La división de ", a, " y ", b, "es = ", a / b)
+
+print("\nThat's all, folks!")
