995b2dc3203fe575250ba4db204f7838a0f9dd26
rNodos <93883322+rNodos@users.noreply.github.com>
2025-02-20 14:00:19 +0000

    ejercicios varios

---
+++ Tests/bubble_sort.py
@@ -0,0 +1,40 @@
+#Ordenamiento burbuja
+"""
+my_list = []
+swapped = True
+num = int(input("¿Cuántos elementos deseas ordenar?: "))
+
+for i in range(num):
+    val = float(input("Ingresa un elemento de la lista: "))
+    my_list.append(val)
+
+print("\nMi lista desordenada:")
+print(my_list)
+
+while swapped:
+    swapped = False
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            swapped = True
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+print("\nOrdenada:")
+print(my_list)
+"""
+
+# Versión simplificada
+
+my_list = []
+num = int(input("¿Cuántos elementos deseas ordenar?: "))
+
+for i in range(num):
+    val = float(input("Ingresa un elemento de la lista: "))
+    my_list.append(val)
+
+print("\nDesordenada:")
+print(my_list)
+
+my_list.sort()
+
+print("\nOrdenada:")
+print(my_list)
