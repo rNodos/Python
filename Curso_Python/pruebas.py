4aa32fa813a326222e9273202b221b5416dbf500
rNodos <93883322+rNodos@users.noreply.github.com>
2025-03-14 16:02:30 +0000

    ejercicios diarios

--- Curso_Python/pruebas.py
+++ Curso_Python/pruebas.py
@@ -1,1 +1,28 @@
-assert var == 0
+class Timer:
+    def __init__( ??? ):
+        #
+        # Escribir código aquí
+        #
+
+    def __str__(self):
+        #
+        # Escribir código aquí
+        #
+
+    def next_second(self):
+        #
+        # Escribir código aquí
+        #
+
+    def prev_second(self):
+        #
+        # Escribir código aquí
+        #
+
+
+timer = Timer(23, 59, 59)
+print(timer)
+timer.next_second()
+print(timer)
+timer.prev_second()
+print(timer)
